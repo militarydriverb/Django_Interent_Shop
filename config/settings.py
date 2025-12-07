a9ae6257c271a8d460f1b3d0112568f54c9328ed
Eugene Zorin <militarydriverb@gmail.com>
2025-12-07 19:51:23 +0300

    Setup Pillow package, create Category and Product models migrations, recheck server run, update settings.py file

--- config/settings.py
+++ config/settings.py
@@ -14,6 +14,7 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
+load_dotenv(override=True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
