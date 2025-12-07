4d5f8792042b25be11f8e89f7f8d73da0ed23aaf
Eugene Zorin <militarydriverb@gmail.com>
2025-12-07 23:23:14 +0300

    Custom command creation. Database cleaning and reuploading with fixture data

--- config/settings.py
+++ config/settings.py
@@ -131,3 +131,7 @@ STATICFILES_DIRS = (BASE_DIR / "static",)
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = "media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
