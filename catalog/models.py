014f31e170c961fd9540feec1d443e175891e6c0
Eugene Zorin <militarydriverb@gmail.com>
2025-12-07 19:18:43 +0300

    Product and Category models slightly update

--- catalog/models.py
+++ catalog/models.py
@@ -12,13 +12,20 @@ class Category(models.Model):
         verbose_name='Category Description',
         help_text='Enter Category Description',
     )
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Createion Date',
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Last Update Date',
+    )
 
 
     class Meta:
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
+        ordering = ['name']
 
     def __str__(self):
             return self.name
@@ -49,8 +56,14 @@ class Product(models.Model):
         related_name='products'
         )
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Creation Date',
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Last Update Date',
+    )
 
     class Meta:
         verbose_name = 'Product'
