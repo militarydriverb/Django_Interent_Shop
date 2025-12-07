62315ae7ddd58989230d2040dd2912a3af7afe3b
Eugene Zorin <militarydriverb@gmail.com>
2025-12-07 19:25:21 +0300

    Product and Category models slightly update, linter black setup and run

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('contacts/', views.contacts, name='contacts'),
+    path("", views.home, name="home"),
+    path("contacts/", views.contacts, name="contacts"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -3,13 +3,13 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
 
 
 def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        message = request.POST.get('message')
+    if request.method == "POST":
+        name = request.POST.get("name")
+        message = request.POST.get("message")
 
         return HttpResponse(f"Спасибо {name}! Ваши данные успешно учтены.")
-    return render(request, 'contacts.html')
+    return render(request, "contacts.html")
