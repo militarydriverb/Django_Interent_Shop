870ff2db505de7e5fd35567641e8ef68b266e497
Eugene Zorin <militarydriverb@gmail.com>
2025-12-20 21:58:11 +0300

    Task 1, Task 2, Task 3 and Task* point 1 done

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
 from . import views
+from catalog.views import cars, car_detail, add_product, edit_product
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", cars , name="cars"),
+    path("home/", views.home, name="home"),
     path("contacts/", views.contacts, name="contacts"),
+    path("cars/<int:pk>/", car_detail , name="car_detail"),
+    path('add/', add_product, name='add_product'),
+    path('edit/<int:pk>/', edit_product, name='edit_product'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+
+from catalog.apps import CatalogConfig
+from catalog.forms import ProductForm
+from catalog.models import Product
 
 
 def home(request):
@@ -13,3 +17,39 @@ def contacts(request):
 
         return HttpResponse(f"Спасибо {name}! Ваши данные успешно учтены.")
     return render(request, "contacts.html")
+
+
+def cars(request):
+    cars  = Product.objects.all()
+    context = {"cars": cars}
+    return render(request, "cars_list.html", context)
+
+
+def car_detail(request,pk):
+    car = get_object_or_404(Product, pk=pk)
+    context = {"car": car}
+    return render(request, "car_detail.html", context)
+
+
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('/')  # перенаправляем обратно на главную страницу
+    else:
+        form = ProductForm()
+    return render(request, 'add_product.html', {'form': form})
+
+
+def edit_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('catalog:cars')  # перенаправляем на список продуктов
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'edit_product.html', {'form': form})
+
