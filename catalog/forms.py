cfeba6ebfc73d60212b178976115ac57ffb3fd4a
Eugene Zorin <militarydriverb@gmail.com>
2026-01-13 23:24:43 +0300

    Implementation of task 3 and Star* task

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.forms.fields import BooleanField
+
 from .models import Product
 
 FORBIDDEN_WORDS = [
@@ -13,7 +15,18 @@ FORBIDDEN_WORDS = [
     "радар",
 ]
 
-class ProductForm(forms.ModelForm):
+class StyleFormMixin():
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                field.widget.attrs['class'] = 'form-control'
+
+
+
+class ProductForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Product
         fields = ("name", "description", "image", "price")
@@ -38,3 +51,17 @@ class ProductForm(forms.ModelForm):
         if not isinstance(price, (int, float)) or price <= 0:
             raise forms.ValidationError("Price must be a positive number.")
         return price
+
+    def clean_image(self):
+        image = self.cleaned_data.get("image")
+        if image:
+            # Проверка размера файла (максимум 5 МБ)
+            if image.size > 5 * 1024 * 1024:
+                raise forms.ValidationError("Image size should not exceed 5 MB.")
+            
+            # Проверка формата файла
+            import os
+            ext = os.path.splitext(image.name)[1].lower()
+            if ext not in [".jpg", ".jpeg", ".png"]:
+                raise forms.ValidationError("Only JPEG and PNG formats are permitted.")
+        return image
